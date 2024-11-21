55fe14580afabe74eaec44e185f0a4e609a39991
MGergo20 <medgyaszayg3@gmail.com>
2024-11-21 10:57:21 +0100

    Feladatok

---
+++ feladatok.py
@@ -0,0 +1,17 @@
+def new_york(stadion_lista):
+    db=0
+    for i in range(0,len(stadion_lista),1):
+        if stadion_lista[i].varos == "New York":
+            db+=1
+    return db
+
+
+def csapatszam(stadion_lista):
+    csapat_db=0
+    for i in range(0,len(stadion_lista),1):
+        if 1<= stadion_lista[i].csapatok_szama <=100:
+            csapat_db+=1
+    return csapat_db
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from Stadion import Stadion
 
-import filebeolvasas
+import filebeolvasas, feladatok
 
 stadionok=filebeolvasas.beolvas("stadionok.txt",[])
 for i in range(1,len(stadionok),1):
     print(stadionok[i])
 
 print(f"Első stadion neve:{stadionok[0]}")
+
+
+feladat1=feladatok.new_york(stadionok)
+print(f"A New York-i Stadionok száma: {feladat1}")
+
+feladat2=feladatok.csapatszam(stadionok)
+print(f"Csapatok száma: {feladat2}")
